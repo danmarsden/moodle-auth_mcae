c0590eb0c162e63529c5ad2f3f6d5722d88fcf28
kamat <kamasutra12@yandex.ru>
2015-11-28 16:38:12 +0300

    Update version.php

--- version.php
+++ version.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015112815;
 $plugin->component = 'auth_mcae';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2015051103;
+$plugin->requires = 2015041700;
 $plugin->release = '2.9.3';
